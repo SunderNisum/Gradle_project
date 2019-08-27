d7d78f9bfecb4bc06981d697876b4ae55001a682
Sunder Iswarabhotla <isunder@nisum.com>
2019-08-27 13:07:47 +0530

    After stashing

--- src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
+++ src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
@@ -9,6 +9,10 @@ public class HelloGradleController {
     public String helloGradle() {
         return "Hello Gradle!";
     }
+	/*
+	 * This is newly added from the github
+	 * This should be going through merge
+	 */
 	@GetMapping("/anotherHello")
     public String sayHello() {
         return "Hello World!";
