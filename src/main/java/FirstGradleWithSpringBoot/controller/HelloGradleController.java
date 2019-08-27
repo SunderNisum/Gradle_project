288583ef77013884e3ed02914de8fbd35c40380d
Sunder Iswarabhotla <isunder@nisum.com>
2019-08-27 13:43:02 +0530

    another one

--- src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
+++ src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
@@ -3,10 +3,19 @@ package FirstGradleWithSpringBoot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/api")
 public class HelloGradleController {
-	@GetMapping
+	@GetMapping("/Hello")
     public String helloGradle() {
         return "Hello Gradle!";
+    }
+	/*
+	 * This is newly added from the github
+	 * This should be going through merge
+	 * This is should be reset with head
+	 */
+	@GetMapping("/anotherHello")
+    public String sayHello() {
+        return "Hello World!";
     }
 }
