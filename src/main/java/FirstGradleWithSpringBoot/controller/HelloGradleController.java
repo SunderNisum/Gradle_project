f7f4af4f1bc1f05705d446f3b3c9a6c41b921af5
Sunder Iswarabhotla <isunder@nisum.com>
2019-08-27 12:23:22 +0530

    September 1st release v1

--- src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
+++ src/main/java/FirstGradleWithSpringBoot/controller/HelloGradleController.java
@@ -3,10 +3,14 @@ package FirstGradleWithSpringBoot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/api")
 public class HelloGradleController {
-	@GetMapping
+	@GetMapping("/Hello")
     public String helloGradle() {
         return "Hello Gradle!";
+    }
+	@GetMapping("/anotherHello")
+    public String sayHello() {
+        return "Hello World!";
     }
 }
